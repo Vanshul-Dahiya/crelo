f49db3e195a756e32a94517a4e24aac7c9cc081a
Vanshul Dahiya <vanshuldahiya2002@gmail.com>
2024-01-17 10:06:13 +0530

    create organization + middleware route protection

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,10 @@
+
+const OrganizationIdPage = () => {
+    return (
+        <div>
+            org page
+        </div>
+    )
+}
+
+export default OrganizationIdPage
--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
@@ -6,6 +7,32 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   // allow users to visit landing page without ever signing in
   publicRoutes: ["/"],
+  afterAuth(auth, req) {
+    // if we are logged in and we are on landing page -> redirect
+    if (auth.userId && auth.isPublicRoute) {
+      let path = "/select-org";
+
+      if (auth.orgId) {
+        path = `/organization/${auth.orgId}`;
+      }
+
+      const orgSelection = new URL(path, req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+
+    // return user to url they were trying to access without signing in..after he signs in
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({
+        returnBackUrl: req.url,
+      });
+    }
+
+    // user is logged in but don't have an org. then force him to create a org
+    if (auth.userId && !auth.orgId && req.nextUrl.pathname !== "/select-org") {
+      const orgSelection = new URL("/select-org", req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+  },
 });
 
 export const config = {
