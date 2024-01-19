d6cdca7e4671b68dc59a369850b7d7273b491687
Vanshul Dahiya <vanshuldahiya2002@gmail.com>
2024-01-19 17:18:22 +0530

    added image + title in database and updated DB schema accordingly

--- components/form/form-picker.tsx
+++ components/form/form-picker.tsx
@@ -3,12 +3,13 @@
 import { useEffect, useState } from "react"
 
 import { unsplash } from "@/lib/unsplash"
-import { Loader2 } from "lucide-react"
+import { Check, Loader2 } from "lucide-react"
 import { useFormStatus } from "react-dom"
 import { cn } from "@/lib/utils"
 import Image from "next/image"
 import { defaultImages } from "@/constants/images"
 import Link from "next/link"
+import { FormErrors } from "./form-errors"
 
 interface FormPickerProps {
     id: string,
@@ -59,33 +60,43 @@ export const FormPicker = ({
         <div className="relative">
             <div className="grid grid-cols-3 gap-2 mb-2">
                 {
-                    images.map((img) => (
-                        <div key={img.id}
+                    images.map((image) => (
+                        <div key={image.id}
                             className={cn("cursor-pointer relative aspect-video group hover:opacity-75 transition bg-muted",
                                 pending && "opacity-50 hover:opacity-50 cursor-auto")}
                             onClick={() => {
                                 if (pending) return;
-                                setSelectedImageId(img.id)
+                                setSelectedImageId(image.id)
                             }}
                         >
+                            <input
+                                type="radio" name={id} id={id} className="hidden"
+                                checked={selectedImageId === image.id} disabled={pending}
+                                value={`${image.id} | ${image.urls.thumb} | ${image.urls.full} | ${image.links.html} | ${image.user.name} }`}
+                            />
                             <Image
-                                src={img.urls.thumb}
+                                src={image.urls.thumb}
                                 alt="unsplash image"
                                 fill
                                 className="object-cover rounded-sm"
                             />
+                            {selectedImageId === image.id && (
+                                <div className="absolute inset-y-0 h-full w-full bg-black/30 flex items-center justify-center">
+                                    <Check className="h-4 w-4 text-white" />
+                                </div>
+                            )}
                             <Link
-                                href={img.links.html}
+                                href={image.links.html}
                                 target="_blank"
                                 className="opacity-0 group-hover:opacity-100 absolute bottom-0 w-full text-[10px] truncate text-white hover:underline p-1 bg-black/50"
                             >
-                                {img.user.name}
+                                {image.user.name}
                             </Link>
                         </div>
                     ))
                 }
-
             </div>
+            <FormErrors id="image" errors={errors} />
         </div>
     )
 }
