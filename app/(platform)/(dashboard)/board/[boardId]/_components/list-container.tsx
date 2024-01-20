8b6e89dc2a17ee45448966ca06b5f8ee9a72f918
Vanshul Dahiya <vanshuldahiya2002@gmail.com>
2024-01-20 19:02:12 +0530

    list items displayed with  list header component

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -2,14 +2,27 @@
 
 import { ListWithCards } from "@/types";
 import { ListForm } from "./list-form";
+import { useEffect, useState } from "react";
+import { ListItem } from "./list-item";
 
 interface ListContainerProps {
     data: ListWithCards[];
     boardId: string
 }
 export const ListContainer = ({ data, boardId }: ListContainerProps) => {
+    // update data in real time for drag and drop
+    const [orderedData, setOrderedData] = useState(data)
+    useEffect(() => {
+        setOrderedData(data)
+    }, [data])
+
     return (
-        <ol>
+        <ol className="flex gap-x-3 h-full">
+            {orderedData.map((list, index) => {
+                return (
+                    <ListItem key={list.id} index={index} data={list} />
+                )
+            })}
             <ListForm />
             <div className="flex-shrink-0 w-1" />
         </ol>
