51437e06cb6cbccc79576ab30307535795a1c851
Vanshul Dahiya <vanshuldahiya2002@gmail.com>
2024-01-22 21:50:54 +0530

    complete - [stripe isPro subscription problem]

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -8,6 +8,7 @@ import Link from "next/link"
 import { Skeleton } from "@/components/ui/skeleton"
 import { getAvailableCount } from "@/lib/org-limit"
 import { MAX_FREE_BOARDS } from "@/constants/boards"
+import { checkSubscription } from "@/lib/subscription"
 
 export const BoardList = async () => {
 
@@ -27,6 +28,7 @@ export const BoardList = async () => {
     })
 
     const availableCount = await getAvailableCount()
+    const isPro = await checkSubscription()
 
     return (
         <div className="space-y-4">
@@ -53,7 +55,7 @@ export const BoardList = async () => {
                     <div role="button" className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition">
                         <p className="text-sm">Create New Board</p>
                         <span className="text-xs">
-                            {`${MAX_FREE_BOARDS - availableCount} remaining!`}
+                            {isPro ? "Unlimited" : `${MAX_FREE_BOARDS - availableCount} remaining!`}
                         </span>
                         <Hint
                             sideOffset={40}
