09a1176320f6ac78ac906a8bcb46f3efe237f7e3
Vanshul Dahiya <vanshuldahiya2002@gmail.com>
2024-01-22 15:13:41 +0530

    schema updated + board limit added

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -6,6 +6,8 @@ import { auth } from "@clerk/nextjs"
 import { redirect } from "next/navigation"
 import Link from "next/link"
 import { Skeleton } from "@/components/ui/skeleton"
+import { getAvailableCount } from "@/lib/org-limit"
+import { MAX_FREE_BOARDS } from "@/constants/boards"
 
 export const BoardList = async () => {
 
@@ -24,6 +26,7 @@ export const BoardList = async () => {
         }
     })
 
+    const availableCount = await getAvailableCount()
 
     return (
         <div className="space-y-4">
@@ -50,7 +53,7 @@ export const BoardList = async () => {
                     <div role="button" className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition">
                         <p className="text-sm">Create New Board</p>
                         <span className="text-xs">
-                            5 remaining!
+                            {`${MAX_FREE_BOARDS - availableCount} remaining!`}
                         </span>
                         <Hint
                             sideOffset={40}
